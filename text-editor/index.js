53c992b33b3c2477153def4e7049d0b6496846f0
Bill Ni <bill.8ni@gmail.com>
2022-03-03 13:59:34 -0500

    update

---
+++ text-editor/index.js
@@ -0,0 +1,23 @@
+var download = (filename, text) => {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+    e.preventDefault();
+    download("text.txt", document.getElementById("text").value);
+  }
+}, false);
+
+document.getElementById("btn").addEventListener("click", function(e) {
+  download("text.txt", document.getElementById("text").value);
+});
