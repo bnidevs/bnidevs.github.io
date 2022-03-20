ba37c38c6b460d36acd82eae812de1a9f69e728e
Bill Ni <bill.8ni@gmail.com>
2022-03-19 23:08:28 -0400

    add download button

--- get-my-commits/index.js
+++ get-my-commits/index.js
@@ -1,5 +1,25 @@
+var name = "";
+var projname = "";
+
+var download = (filename, text) => {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
 document.getElementById("copylinks").addEventListener("click", () => {
 	navigator.clipboard.writeText(document.getElementById("commitlinks").innerText);
+});
+
+document.getElementById("download").addEventListener("click", () => {
+	download("contributions.txt", name + (name.length == 0 ? "" : "\n") + projname + "\n\nCommits:\n" + document.getElementById("commitlinks").innerText);
 });
 
 var main = () => {
@@ -9,6 +29,8 @@ var main = () => {
 		var repolink = document.getElementById("inputrepolink").value;
 		let owner = repolink.substring(repolink.indexOf("github.com") + 11, repolink.indexOf("/", repolink.indexOf("github.com") + 11));
 		let repo = repolink.substring(repolink.indexOf(owner) + owner.length + 1);
+
+		projname = repo;
 
 		if(repo.charAt(repo.length - 1) == "/"){
 			repo = repo.substring(0, repo.length - 1);
@@ -30,6 +52,7 @@ var main = () => {
 					document.getElementById("commitlinks").innerHTML += '<a href="' + data[i]["html_url"] + '">' + data[i]["html_url"] + "</a>" + "<br>";
 				}
 				lastsha = data[data.length - 1]["sha"];
+				name = data[0]["commit"]["author"]["name"];
 			});
 
 		while(k > 1){
@@ -45,6 +68,8 @@ var main = () => {
 		}
 
 		document.getElementById("copylinks").style.display = "block";
+
+		document.getElementById("download").style.display = "block";
 
 		return true;
 	}catch (error){
