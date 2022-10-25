f87041a414b8cf4fee4df6f542c9b00ced01edf0
bnidevs <bnidevs@users.noreply.github.com>
2022-10-25 18:33:56 +0000

    Prettified Code!

--- get-my-commits/datepicker.js
+++ get-my-commits/datepicker.js
@@ -1,6 +1,11 @@
-document.getElementById("datepicker").max = new Date().toISOString().split("T")[0];
-document.getElementById("datepicker").min = new Date().toISOString().split("-")[0] + "-01-01";
+document.getElementById("datepicker").max = new Date()
+  .toISOString()
+  .split("T")[0];
+document.getElementById("datepicker").min =
+  new Date().toISOString().split("-")[0] + "-01-01";
 
-document.getElementById("datecheckbox").addEventListener('change', (e) => {
-    document.getElementById("datepicker").style.display = e.target.checked ? "block" : "none";
+document.getElementById("datecheckbox").addEventListener("change", (e) => {
+  document.getElementById("datepicker").style.display = e.target.checked
+    ? "block"
+    : "none";
 });
