6764ae28c4cfcf362cc1cdfaed46446e603d4ab7
bnidevs <bnidevs@users.noreply.github.com>
2022-10-25 18:34:33 +0000

    Prettified Code!

--- get-my-commits/index.js
+++ get-my-commits/index.js
@@ -170,7 +170,7 @@ var main = () => {
         "contents.xcworkspacedata",
         "IDEWorkspaceChecks.plist",
         "Package.resolved",
-        "yarn.lock"
+        "yarn.lock",
       ];
 
       await fetch(commitlink, {
