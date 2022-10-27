dbfd48ecc0af8fd6a264d18aefcf2a88ca35ddba
bnidevs <bnidevs@users.noreply.github.com>
2022-10-27 04:00:09 +0000

    Prettified Code!

--- get-my-commits/index.js
+++ get-my-commits/index.js
@@ -329,11 +329,11 @@ var main = () => {
                 console.log(commitlinks);
 
                 let linktosha = {};
-                Object.keys(shatolink).forEach(sha => {
+                Object.keys(shatolink).forEach((sha) => {
                   linktosha[shatolink[sha]] = sha;
                 });
-                Object.keys(commitlinks).forEach(lnk => {
-                  if(!(lnk in linktosha)){
+                Object.keys(commitlinks).forEach((lnk) => {
+                  if (!(lnk in linktosha)) {
                     delete commitlinks[lnk];
                   }
                 });
