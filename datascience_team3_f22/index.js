8937d24be86028a55ab37dd456c644d1d1f8ba5c
bnidevs <bnidevs@users.noreply.github.com>
2022-11-20 02:18:58 +0000

    Prettified Code!

--- datascience_team3_f22/index.js
+++ datascience_team3_f22/index.js
@@ -1,26 +1,37 @@
-document.getElementById('img_container').innerHTML += `<img src="https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/map.png?raw=true" alt="Map" class="overlay">`
+document.getElementById(
+  "img_container"
+).innerHTML += `<img src="https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/map.png?raw=true" alt="Map" class="overlay">`;
 
 let overlays = {
-    'Population': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/population_count/population.png?raw=true',
-    'Wind Speed': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/energy_strength/wind/wind.png?raw=true',
-    'Earthquakes': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/earthquakes/earthquakes.png?raw=true',
-    'Tsunamis': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/tsunamis/tsunamis.png?raw=true',
-    'Hurricanes': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/hurricanes/hurricanes.png?raw=true'
-}
+  Population:
+    "https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/population_count/population.png?raw=true",
+  "Wind Speed":
+    "https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/energy_strength/wind/wind.png?raw=true",
+  Earthquakes:
+    "https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/earthquakes/earthquakes.png?raw=true",
+  Tsunamis:
+    "https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/tsunamis/tsunamis.png?raw=true",
+  Hurricanes:
+    "https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/hurricanes/hurricanes.png?raw=true",
+};
 
 for (let overlay in overlays) {
-    document.getElementById('img_container').innerHTML += `<img id="${overlay}" src="${overlays[overlay]}" alt="${overlay}" class="hidden overlay">`
-    document.getElementById('toggle_menu').innerHTML += `<input type="checkbox" id="${overlay}_toggle" name="${overlay}"><label for="${overlay}">${overlay}</label><br>`
+  document.getElementById(
+    "img_container"
+  ).innerHTML += `<img id="${overlay}" src="${overlays[overlay]}" alt="${overlay}" class="hidden overlay">`;
+  document.getElementById(
+    "toggle_menu"
+  ).innerHTML += `<input type="checkbox" id="${overlay}_toggle" name="${overlay}"><label for="${overlay}">${overlay}</label><br>`;
 }
 
-let toggles = document.querySelectorAll('input[type=checkbox]')
+let toggles = document.querySelectorAll("input[type=checkbox]");
 for (let toggle of toggles) {
-    toggle.addEventListener('change', function() {
-        let overlay = document.getElementById(toggle.name)
-        if (toggle.checked) {
-            overlay.classList.remove('hidden')
-        } else {
-            overlay.classList.add('hidden')
-        }
-    })
+  toggle.addEventListener("change", function () {
+    let overlay = document.getElementById(toggle.name);
+    if (toggle.checked) {
+      overlay.classList.remove("hidden");
+    } else {
+      overlay.classList.add("hidden");
+    }
+  });
 }
--- kanban/index.js
+++ kanban/index.js
@@ -3,14 +3,14 @@ let cookie = document.cookie;
 const LEFT = "L";
 const RIGHT = "R";
 
-const directions = {"L": -1, "R": 1}
+const directions = { L: -1, R: 1 };
 
-const categories = {"todo": 0, "inprogress": 1, "done": 2};
-const inv_categories = {0: "todo", 1: "inprogress", 2: "done"};
+const categories = { todo: 0, inprogress: 1, done: 2 };
+const inv_categories = { 0: "todo", 1: "inprogress", 2: "done" };
 
 let tasks;
 let resettasks = () => {
-    tasks = {"todo":[], "inprogress":[], "done": []};
+  tasks = { todo: [], inprogress: [], done: [] };
 };
 resettasks();
 
@@ -29,131 +29,159 @@ const TEMPLATE_item = `
 `;
 
 let HTMLgen = (s) => {
-    let placeholder_elem = document.createElement("div");
-    placeholder_elem.innerHTML = s;
-    return placeholder_elem.firstChild;
-} 
+  let placeholder_elem = document.createElement("div");
+  placeholder_elem.innerHTML = s;
+  return placeholder_elem.firstChild;
+};
 
-if(cookie !== ""){
-    try{
-        tasks = JSON.parse(cookie);
-    }catch(err){
-        alert("Cookie corrupted, tasks reset");
-        resettasks();
-    }
+if (cookie !== "") {
+  try {
+    tasks = JSON.parse(cookie);
+  } catch (err) {
+    alert("Cookie corrupted, tasks reset");
+    resettasks();
+  }
 }
 
-let deltask = (e, k=null) => {
-    let pk;
-    if(k !== null){
-        pk = k;
-    }else{
-        pk = e.target;
-    }
+let deltask = (e, k = null) => {
+  let pk;
+  if (k !== null) {
+    pk = k;
+  } else {
+    pk = e.target;
+  }
 
-    const item_txt = pk.parentNode.parentNode.getElementsByClassName("item_txt")[0].innerText;
-    let col = pk;
-    while(!col.id.endsWith("_col")){
+  const item_txt =
+    pk.parentNode.parentNode.getElementsByClassName("item_txt")[0].innerText;
+  let col = pk;
+  while (!col.id.endsWith("_col")) {
+    col = col.parentNode;
+  }
+
+  const col_categ = col.id.split("_")[0];
+
+  tasks[col_categ].splice(tasks[col_categ].indexOf(item_txt), 1);
+
+  col.removeChild(pk.parentNode.parentNode.parentNode);
+
+  savetasks();
+};
+
+let edittask = (e) => {
+  const item_txt =
+    e.target.parentNode.parentNode.getElementsByClassName("item_txt")[0]
+      .innerText;
+  var field = document.createElement("input");
+  field.value = item_txt;
+  field.classList.add("item_txt");
+  field.addEventListener("keydown", (e) => {
+    if (e.enterKey) {
+      let col = e.target;
+      while (!col.id.endsWith("_col")) {
         col = col.parentNode;
+      }
+
+      const col_categ = col.id.split("_")[0];
+
+      tasks[col_categ][tasks[col_categ].indexOf(item_txt)] = field.value;
+      e.target.parentNode.replaceChild(
+        HTMLgen(`<p class="item_txt">${field.value}</p>`),
+        e.target
+      );
+    }
+  });
+  field.addEventListener("blur", (e) => {
+    let col = e.target;
+    while (!col.id.endsWith("_col")) {
+      col = col.parentNode;
     }
 
     const col_categ = col.id.split("_")[0];
 
-    tasks[col_categ].splice(tasks[col_categ].indexOf(item_txt), 1);
+    tasks[col_categ][tasks[col_categ].indexOf(item_txt)] = field.value;
+    e.target.parentNode.replaceChild(
+      HTMLgen(`<p class="item_txt">${field.value}</p>`),
+      e.target
+    );
+  });
 
-    col.removeChild(pk.parentNode.parentNode.parentNode);
+  e.target.parentNode.parentNode.replaceChild(
+    field,
+    e.target.parentNode.parentNode.getElementsByClassName("item_txt")[0]
+  );
 
-    savetasks();
-}
-
-let edittask = (e) => {
-    const item_txt = e.target.parentNode.parentNode.getElementsByClassName("item_txt")[0].innerText;
-    var field = document.createElement("input");
-    field.value = item_txt;
-    field.classList.add("item_txt")
-    field.addEventListener("keydown", (e) => {
-        if(e.enterKey){
-            let col = e.target;
-            while(!col.id.endsWith("_col")){
-                col = col.parentNode;
-            }
-        
-            const col_categ = col.id.split("_")[0];
-        
-            tasks[col_categ][tasks[col_categ].indexOf(item_txt)] = field.value;
-            e.target.parentNode.replaceChild(HTMLgen(`<p class="item_txt">${field.value}</p>`), e.target);
-        }
-    });
-    field.addEventListener("blur", (e) => {
-        let col = e.target;
-        while(!col.id.endsWith("_col")){
-            col = col.parentNode;
-        }
-    
-        const col_categ = col.id.split("_")[0];
-    
-        tasks[col_categ][tasks[col_categ].indexOf(item_txt)] = field.value;
-        e.target.parentNode.replaceChild(HTMLgen(`<p class="item_txt">${field.value}</p>`), e.target);
-    });
-
-    e.target.parentNode.parentNode.replaceChild(field, e.target.parentNode.parentNode.getElementsByClassName("item_txt")[0]);
-
-    savetasks();
-}
+  savetasks();
+};
 
 let movetask = (editbtn, currcol, dir) => {
-    const item_txt = editbtn.parentNode.parentNode.getElementsByClassName("item_txt")[0].innerText;
-    if((currcol === "todo" && dir === LEFT) || (currcol === "done" && dir === RIGHT)){
-        return;
-    }
+  const item_txt =
+    editbtn.parentNode.parentNode.getElementsByClassName("item_txt")[0]
+      .innerText;
+  if (
+    (currcol === "todo" && dir === LEFT) ||
+    (currcol === "done" && dir === RIGHT)
+  ) {
+    return;
+  }
 
-    deltask(null, editbtn);
+  deltask(null, editbtn);
 
-    addtask(nextcol, item_txt);
+  addtask(nextcol, item_txt);
 
-    savetasks();
-}
+  savetasks();
+};
 
-let addtask = (whichcol, tasktext="", fromsave=false) => {
-    
-    // VISUAL
-    
-    let col = document.getElementById(`${whichcol}_col`);
-    col.innerHTML += TEMPLATE_item;
-    let colitems = col.getElementsByClassName("kanban_item");
-    let latest = colitems[colitems.length - 1];
+let addtask = (whichcol, tasktext = "", fromsave = false) => {
+  // VISUAL
 
-    latest.getElementsByClassName("item_txt")[0].innerText = tasktext;
+  let col = document.getElementById(`${whichcol}_col`);
+  col.innerHTML += TEMPLATE_item;
+  let colitems = col.getElementsByClassName("kanban_item");
+  let latest = colitems[colitems.length - 1];
 
-    latest.getElementsByClassName("del_btn")[0].addEventListener("click", deltask);
-    latest.getElementsByClassName("edit_btn")[0].addEventListener("click", edittask);
+  latest.getElementsByClassName("item_txt")[0].innerText = tasktext;
 
-    latest.getElementsByClassName("left")[0].addEventListener("click", (e) => movetask(e.target, whichcol, LEFT));
-    latest.getElementsByClassName("right")[0].addEventListener("click", (e) => movetask(e.target, whichcol, RIGHT));
+  latest
+    .getElementsByClassName("del_btn")[0]
+    .addEventListener("click", deltask);
+  latest
+    .getElementsByClassName("edit_btn")[0]
+    .addEventListener("click", edittask);
 
-    // LOGICAL
-    
-    if(!fromsave){
-        tasks[whichcol].push(tasktext);
-    }
+  latest
+    .getElementsByClassName("left")[0]
+    .addEventListener("click", (e) => movetask(e.target, whichcol, LEFT));
+  latest
+    .getElementsByClassName("right")[0]
+    .addEventListener("click", (e) => movetask(e.target, whichcol, RIGHT));
 
-    savetasks();
-}
+  // LOGICAL
+
+  if (!fromsave) {
+    tasks[whichcol].push(tasktext);
+  }
+
+  savetasks();
+};
 
 let loadtasks = () => {
-    Object.keys(tasks).forEach((col) => {
-        col.forEach((t) => addtask(col, t, true));
-    });
-}
+  Object.keys(tasks).forEach((col) => {
+    col.forEach((t) => addtask(col, t, true));
+  });
+};
 
 let savetasks = () => {
-    document.cookie = JSON.stringify(tasks);
+  document.cookie = JSON.stringify(tasks);
 
-    Object.keys(categories).forEach((k) => {
-        document.getElementById(`${k}_add`).addEventListener("click", () => addtask(k, ""));
-    });
-}
+  Object.keys(categories).forEach((k) => {
+    document
+      .getElementById(`${k}_add`)
+      .addEventListener("click", () => addtask(k, ""));
+  });
+};
 
 Object.keys(categories).forEach((k) => {
-    document.getElementById(`${k}_add`).addEventListener("click", () => addtask(k, ""));
+  document
+    .getElementById(`${k}_add`)
+    .addEventListener("click", () => addtask(k, ""));
 });
