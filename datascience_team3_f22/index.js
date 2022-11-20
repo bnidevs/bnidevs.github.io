26af0c56b6433cd6de5d9447d41668bc364452e7
Bill Ni <bill.8ni@gmail.com>
2022-11-19 21:18:29 -0500

    update data vis

--- datascience_team3_f22/index.js
+++ datascience_team3_f22/index.js
@@ -1,182 +1,26 @@
-// The svg
-const svg = d3.select("svg"),
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
+document.getElementById('img_container').innerHTML += `<img src="https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/map.png?raw=true" alt="Map" class="overlay">`
 
-// Map and projection
-const path = d3.geoPath();
-const projection = d3.geoMercator().scale(150).center([0, 20]);
+let overlays = {
+    'Population': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/population_count/population.png?raw=true',
+    'Wind Speed': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/energy_strength/wind/wind.png?raw=true',
+    'Earthquakes': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/earthquakes/earthquakes.png?raw=true',
+    'Tsunamis': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/tsunamis/tsunamis.png?raw=true',
+    'Hurricanes': 'https://github.com/ITWSDataScience/Group3_Fall2022/blob/main/natural_disasters/hurricanes/hurricanes.png?raw=true'
+}
 
-// Data and color scale
-const popdensity = new Map();
-const population = new Map();
-const colorScale = d3
-  .scaleThreshold()
-  .domain([1, 5, 10, 20, 40, 100])
-  .range(d3.schemeBlues[7]);
+for (let overlay in overlays) {
+    document.getElementById('img_container').innerHTML += `<img id="${overlay}" src="${overlays[overlay]}" alt="${overlay}" class="hidden overlay">`
+    document.getElementById('toggle_menu').innerHTML += `<input type="checkbox" id="${overlay}_toggle" name="${overlay}"><label for="${overlay}">${overlay}</label><br>`
+}
 
-let optionmap = {
-  popdensity: popdensity,
-  population: population,
-};
-
-let optionname = {
-  popdensity: "Population Density",
-  population: "Population",
-};
-
-let optionunit = {
-  popdensity: "(people per sq km)",
-  population: "(millions)",
-};
-
-let tooltip = d3.select("#tooltip");
-let tooltip_bg = d3
-  .select("#tooltip_bg")
-  .attr("fill", "#FFF")
-  .attr("height", "3em")
-  .style("stroke", "black")
-  .style("opacity", 0);
-
-// Load external data and boot
-Promise.all([
-  d3.json(
-    "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-  ),
-  d3.csv(
-    "https://datasci-general.s3.amazonaws.com/SYB64_1_202110_Population%2C%2BSurface%2BArea%2Band%2BDensity_cleaned.csv",
-    function (d) {
-      if (d.Series === "Population density") {
-        popdensity.set(d.Country, +d.Value);
-      }
-
-      if (d.Series === "Population mid-year estimates (millions)") {
-        population.set(d.Country, +d.Value);
-      }
-    }
-  ),
-]).then(function (loadData) {
-  d3.select("#opts").on("change", function () {
-    let whichdata = d3.select("#opts").property("value");
-
-    let topo = loadData[0];
-
-    let mouseOver = function (d) {
-      d3.selectAll(".Country").transition().duration(200).style("opacity", 0.5);
-      d3.select(this).transition().duration(200).style("opacity", 1);
-      tooltip_bg.style("opacity", 1);
-      tooltip.style("opacity", 1);
-    };
-
-    let mouseLeave = function (d) {
-      d3.selectAll(".Country").transition().duration(200).style("opacity", 0.8);
-      tooltip.style("opacity", 0);
-      tooltip_bg.style("opacity", 0);
-    };
-
-    let mouseMove = function (event, d) {
-      const country_name = d3.select("#country_name");
-      country_name.text(`${d.properties.name}`);
-
-      const country_data = d3.select("#country_data");
-      country_data.text(
-        `${optionname[whichdata]}: ${d.total} ${optionunit[whichdata]}`
-      );
-
-      const [x, y] = d3.pointer(event);
-      tooltip.attr("transform", `translate(${x},${y + 40})`);
-      tooltip_bg.attr(
-        "width",
-        Math.max(
-          d3.select("#country_data").node().getBBox().width,
-          d3.select("#country_name").node().getBBox().width
-        )
-      );
-
-      tooltip.style("opacity", 1);
-      tooltip_bg.style("opacity", 1);
-    };
-
-    svg.select("#plotg").selectAll("path").remove();
-
-    svg
-      .select("#plotg")
-      .selectAll("path")
-      .data(topo.features)
-      .enter()
-      .append("path")
-      .attr("d", d3.geoPath().projection(projection))
-      .attr("fill", function (d) {
-        d.total = optionmap[whichdata].get(d.properties.name) || 0;
-        return colorScale(d.total);
-      })
-      .style("stroke", "transparent")
-      .attr("class", function (d) {
-        return "Country";
-      })
-      .style("opacity", 0.8)
-      .on("mouseover", mouseOver)
-      .on("mouseleave", mouseLeave)
-      .on("mousemove", mouseMove);
-  });
-
-  let whichdata = "population";
-
-  let topo = loadData[0];
-
-  let mouseOver = function (d) {
-    d3.selectAll(".Country").transition().duration(200).style("opacity", 0.5);
-    d3.select(this).transition().duration(200).style("opacity", 1);
-    tooltip.style("opacity", 1);
-    tooltip_bg.style("opacity", 1);
-  };
-
-  let mouseLeave = function (d) {
-    d3.selectAll(".Country").transition().duration(200).style("opacity", 0.8);
-    tooltip.style("opacity", 0);
-    tooltip_bg.style("opacity", 0);
-  };
-
-  let mouseMove = function (event, d) {
-    const country_name = d3.select("#country_name");
-    country_name.text(`${d.properties.name}`);
-
-    const country_data = d3.select("#country_data");
-    country_data.text(
-      `${optionname[whichdata]}: ${d.total} ${optionunit[whichdata]}`
-    );
-
-    const [x, y] = d3.pointer(event);
-    tooltip.attr("transform", `translate(${x},${y + 40})`);
-    tooltip_bg.attr(
-      "width",
-      Math.max(
-        d3.select("#country_data").node().getBBox().width,
-        d3.select("#country_name").node().getBBox().width
-      )
-    );
-
-    tooltip.style("opacity", 1);
-    tooltip_bg.style("opacity", 1);
-  };
-
-  svg
-    .select("#plotg")
-    .selectAll("path")
-    .data(topo.features)
-    .enter()
-    .append("path")
-    .attr("d", d3.geoPath().projection(projection))
-    .attr("fill", function (d) {
-      d.total = optionmap[whichdata].get(d.properties.name) || 0;
-      return colorScale(d.total);
+let toggles = document.querySelectorAll('input[type=checkbox]')
+for (let toggle of toggles) {
+    toggle.addEventListener('change', function() {
+        let overlay = document.getElementById(toggle.name)
+        if (toggle.checked) {
+            overlay.classList.remove('hidden')
+        } else {
+            overlay.classList.add('hidden')
+        }
     })
-    .style("stroke", "transparent")
-    .attr("class", function (d) {
-      return "Country";
-    })
-    .style("opacity", 0.8)
-    .on("mouseover", mouseOver)
-    .on("mouseleave", mouseLeave)
-    .on("mousemove", mouseMove);
-});
+}
---
+++ kanban/index.js
@@ -0,0 +1,159 @@
+let cookie = document.cookie;
+
+const LEFT = "L";
+const RIGHT = "R";
+
+const directions = {"L": -1, "R": 1}
+
+const categories = {"todo": 0, "inprogress": 1, "done": 2};
+const inv_categories = {0: "todo", 1: "inprogress", 2: "done"};
+
+let tasks;
+let resettasks = () => {
+    tasks = {"todo":[], "inprogress":[], "done": []};
+};
+resettasks();
+
+const TEMPLATE_item = `
+<div class="kanban_item flex">
+    <button class="move_btn left"><</button>
+    <div class="item_ctnr flex">
+        <p class="item_txt"></p>
+        <div class="item_fxn_col flex">
+            <button class="edit_btn">Edit</button>
+            <button class="del_btn">Delete</button>
+        </div>
+    </div>
+    <button class="move_btn right">></button>
+</div>
+`;
+
+let HTMLgen = (s) => {
+    let placeholder_elem = document.createElement("div");
+    placeholder_elem.innerHTML = s;
+    return placeholder_elem.firstChild;
+} 
+
+if(cookie !== ""){
+    try{
+        tasks = JSON.parse(cookie);
+    }catch(err){
+        alert("Cookie corrupted, tasks reset");
+        resettasks();
+    }
+}
+
+let deltask = (e, k=null) => {
+    let pk;
+    if(k !== null){
+        pk = k;
+    }else{
+        pk = e.target;
+    }
+
+    const item_txt = pk.parentNode.parentNode.getElementsByClassName("item_txt")[0].innerText;
+    let col = pk;
+    while(!col.id.endsWith("_col")){
+        col = col.parentNode;
+    }
+
+    const col_categ = col.id.split("_")[0];
+
+    tasks[col_categ].splice(tasks[col_categ].indexOf(item_txt), 1);
+
+    col.removeChild(pk.parentNode.parentNode.parentNode);
+
+    savetasks();
+}
+
+let edittask = (e) => {
+    const item_txt = e.target.parentNode.parentNode.getElementsByClassName("item_txt")[0].innerText;
+    var field = document.createElement("input");
+    field.value = item_txt;
+    field.classList.add("item_txt")
+    field.addEventListener("keydown", (e) => {
+        if(e.enterKey){
+            let col = e.target;
+            while(!col.id.endsWith("_col")){
+                col = col.parentNode;
+            }
+        
+            const col_categ = col.id.split("_")[0];
+        
+            tasks[col_categ][tasks[col_categ].indexOf(item_txt)] = field.value;
+            e.target.parentNode.replaceChild(HTMLgen(`<p class="item_txt">${field.value}</p>`), e.target);
+        }
+    });
+    field.addEventListener("blur", (e) => {
+        let col = e.target;
+        while(!col.id.endsWith("_col")){
+            col = col.parentNode;
+        }
+    
+        const col_categ = col.id.split("_")[0];
+    
+        tasks[col_categ][tasks[col_categ].indexOf(item_txt)] = field.value;
+        e.target.parentNode.replaceChild(HTMLgen(`<p class="item_txt">${field.value}</p>`), e.target);
+    });
+
+    e.target.parentNode.parentNode.replaceChild(field, e.target.parentNode.parentNode.getElementsByClassName("item_txt")[0]);
+
+    savetasks();
+}
+
+let movetask = (editbtn, currcol, dir) => {
+    const item_txt = editbtn.parentNode.parentNode.getElementsByClassName("item_txt")[0].innerText;
+    if((currcol === "todo" && dir === LEFT) || (currcol === "done" && dir === RIGHT)){
+        return;
+    }
+
+    deltask(null, editbtn);
+
+    addtask(nextcol, item_txt);
+
+    savetasks();
+}
+
+let addtask = (whichcol, tasktext="", fromsave=false) => {
+    
+    // VISUAL
+    
+    let col = document.getElementById(`${whichcol}_col`);
+    col.innerHTML += TEMPLATE_item;
+    let colitems = col.getElementsByClassName("kanban_item");
+    let latest = colitems[colitems.length - 1];
+
+    latest.getElementsByClassName("item_txt")[0].innerText = tasktext;
+
+    latest.getElementsByClassName("del_btn")[0].addEventListener("click", deltask);
+    latest.getElementsByClassName("edit_btn")[0].addEventListener("click", edittask);
+
+    latest.getElementsByClassName("left")[0].addEventListener("click", (e) => movetask(e.target, whichcol, LEFT));
+    latest.getElementsByClassName("right")[0].addEventListener("click", (e) => movetask(e.target, whichcol, RIGHT));
+
+    // LOGICAL
+    
+    if(!fromsave){
+        tasks[whichcol].push(tasktext);
+    }
+
+    savetasks();
+}
+
+let loadtasks = () => {
+    Object.keys(tasks).forEach((col) => {
+        col.forEach((t) => addtask(col, t, true));
+    });
+}
+
+let savetasks = () => {
+    document.cookie = JSON.stringify(tasks);
+
+    Object.keys(categories).forEach((k) => {
+        document.getElementById(`${k}_add`).addEventListener("click", () => addtask(k, ""));
+    });
+}
+
+Object.keys(categories).forEach((k) => {
+    document.getElementById(`${k}_add`).addEventListener("click", () => addtask(k, ""));
+});
