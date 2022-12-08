fbba16d08155368d43d65e6cc71ba5989b0e916f
bnidevs <bnidevs@users.noreply.github.com>
2022-12-08 18:45:44 +0000

    Prettified Code!

--- get-my-commits/toggledark.js
+++ get-my-commits/toggledark.js
@@ -1,11 +1,21 @@
-if(document.cookie != '' && 'dark' in JSON.parse(document.cookie) && document.cookie.dark){
-    document.getElementById("darkmodecheckbox").check();
+if (
+  document.cookie != "" &&
+  "dark" in JSON.parse(document.cookie) &&
+  document.cookie.dark
+) {
+  document.getElementById("darkmodecheckbox").check();
 }
 
 document.getElementById("darkmodecheckbox").addEventListener("change", (e) => {
-        document.getElementsByTagName("body")[0].classList.toggle('darkmode');
-        [...document.getElementsByTagName("input")].map(e => {e.classList.toggle('darkmode')});
-        [...document.getElementsByTagName("button")].map(e => {e.classList.toggle('darkmode')});
+  document.getElementsByTagName("body")[0].classList.toggle("darkmode");
+  [...document.getElementsByTagName("input")].map((e) => {
+    e.classList.toggle("darkmode");
+  });
+  [...document.getElementsByTagName("button")].map((e) => {
+    e.classList.toggle("darkmode");
+  });
 
-        document.cookie = JSON.stringify({dark: document.getElementById("darkmodecheckbox").checked});
-    });
+  document.cookie = JSON.stringify({
+    dark: document.getElementById("darkmodecheckbox").checked,
+  });
+});
