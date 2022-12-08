c86e7f0c7fa2d8aeb0a0b123d85732dbde51bb04
Bill Ni <bill.8ni@gmail.com>
2022-12-08 13:54:04 -0500

    modify dark toggling

--- get-my-commits/toggledark.js
+++ get-my-commits/toggledark.js
@@ -1,21 +1,24 @@
-if (
-  document.cookie != "" &&
-  "dark" in JSON.parse(document.cookie) &&
-  JSON.parse(document.cookie)["dark"]
-) {
-  document.getElementById("darkmodecheckbox").click();
+let toggle = () => {
+    document.getElementsByTagName("body")[0].classList.toggle("darkmode");
+    [...document.getElementsByTagName("input")].map((e) => {
+      e.classList.toggle("darkmode");
+    });
+    [...document.getElementsByTagName("button")].map((e) => {
+      e.classList.toggle("darkmode");
+    });
+  
+    document.cookie = JSON.stringify({
+      dark: document.getElementById("darkmodecheckbox").checked,
+    });
 }
 
-document.getElementById("darkmodecheckbox").addEventListener("change", (e) => {
-  document.getElementsByTagName("body")[0].classList.toggle("darkmode");
-  [...document.getElementsByTagName("input")].map((e) => {
-    e.classList.toggle("darkmode");
-  });
-  [...document.getElementsByTagName("button")].map((e) => {
-    e.classList.toggle("darkmode");
-  });
+if (
+    document.cookie != "" &&
+    "dark" in JSON.parse(document.cookie) &&
+    JSON.parse(document.cookie)["dark"]
+  ) {
+    document.getElementById("darkmodecheckbox").click();
+    toggle();
+}
 
-  document.cookie = JSON.stringify({
-    dark: document.getElementById("darkmodecheckbox").checked,
-  });
-});
+document.getElementById("darkmodecheckbox").addEventListener("change", toggle);
