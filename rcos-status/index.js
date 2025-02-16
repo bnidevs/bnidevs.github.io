926eddb434295db9aa41fd5498a874fba3ce41ba
Bill Ni <bill.8ni@gmail.com>
2025-02-16 06:40:55 -0500

    testing rcos status page

---
+++ rcos-status/index.js
@@ -0,0 +1,32 @@
+const m = {
+    webpage: {
+        hrn: 'Webpage',
+        link: 'https://new.rcos.io'
+    },
+    users: {
+        hrn: 'Users',
+        link: 'https://new.rcos.io/users'
+    },
+    projects: {
+        hrn: 'Projects',
+        link: 'https://new.rcos.io/projects'
+    },
+    meetings: {
+        hrn: 'Meetings',
+        link: 'https://new.rcos.io/meetings'
+    },
+    small_groups: {
+        hrn: 'Small Groups',
+        link: 'https://new.rcos.io/small_groups'
+    },
+    organizations: {
+        hrn: 'Organizations',
+        link: 'https://new.rcos.io/organizations'
+    },
+}
+
+Object.keys(m).map(k => {
+    fetch(m.k.link).then((response)=>{
+        document.querySelector(`.${k}.status`).innerText = response.status;
+    });
+})
